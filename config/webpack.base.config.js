e5b9778879aeb3c38022d4a84e7d72cbf3e85f37
fanwenxiang <fanwenxiang@aiforward.com>
2022-01-05 11:06:45 +0800

    feature:添加webpack base 配置

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -3,3 +3,36 @@
 // css的转换
 // 配置别名
 // 图片处理之类
+
+module.exports = {
+    entry: {
+        app: 'example/src/main.js'
+    },
+    output: {
+        filename: '',
+        outpath: ''
+    },
+    module: {
+        rules: [
+            {
+                test: /.vue$/,
+                use: ['vue-loader']
+            },
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.s(c|a)ss$/,
+                use: 'style-loader!css-loader!scss-loader'
+            },
+            {
+                test: /.css$/,
+                use: 'mini-css-extract-loader!scss-loader'
+            }
+        ]
+    },
+    plugins: [
+
+    ]
+}
